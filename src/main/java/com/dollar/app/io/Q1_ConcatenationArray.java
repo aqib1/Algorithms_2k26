d3ba0918f377f8a626637046a7a6cdaaffe18e67
aqib javed <aqibbutt3078@gmail.com>
2025-09-28 22:15:58 +0100

    Q1 | Get Concatenation

---
+++ src/main/java/com/dollar/app/io/Q1_ConcatenationArray.java
@@ -0,0 +1,17 @@
+package com.dollar.app.io;
+
+public class Q1_ConcatenationArray {
+
+    // Time complexity O(n)
+    // Space complexity O(2n)
+    public int[] getConcatenation(int[] nums) {
+        var result = new int[2 * nums.length];
+
+        for(int i = 0; i < nums.length; i++) {
+            result[i] = nums[i];
+            result[i + nums.length] = nums[i];
+        }
+
+        return result;
+    }
+}

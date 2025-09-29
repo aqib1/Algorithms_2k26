560078d91a2a535344c9871f3b8b741b0bdd4dd5
aqib javed <aqibbutt3078@gmail.com>
2025-09-29 22:49:38 +0100

    Q2 | Suffle Array

---
+++ src/main/java/com/dollar/app/io/Q2_ShuffleArray.java
@@ -0,0 +1,17 @@
+package com.dollar.app.io;
+
+public class Q2_ShuffleArray {
+    // Time O(n)
+    // Space O(n)
+    public int[] shuffle(int[] nums, int n) {
+        int xi = 0, yi = n;
+        var result = new int[nums.length];
+        int i = 0;
+        while(i < result.length) {
+            result[i++] = nums[xi++];
+            result[i++] = nums[yi++];
+        }
+
+        return result;
+    }
+}

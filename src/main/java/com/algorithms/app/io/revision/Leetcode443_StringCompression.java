ede2be788c156f119621ed0faaf9d6d8b4e8b8e6
aqib javed <aqibbutt3078@gmail.com>
2025-10-19 22:48:21 +0100

    String compression

--- src/main/java/com/algorithms/app/io/revision/Leetcode443_StringCompression.java
+++ src/main/java/com/algorithms/app/io/revision/Leetcode443_StringCompression.java
@@ -13,25 +13,26 @@ public class Leetcode443_StringCompression {
     // Time complexity O(n)
     // Space complexity O(1)
     public static int compress(char[] chars) {
-        int ptr = 0;
-        int count = 0;
+       int ptr = 0;
+       int count = 0;
 
-        for (int i = 0; i < chars.length; ) {
-            var current = chars[i];
-            chars[ptr++] = current;
+       for(int i = 0; i < chars.length;) {
+           var current = chars[i];
+           chars[ptr++] = current;
 
-            while (i < chars.length && current == chars[i]) {
-                count++;
-                i++;
-            }
-            if (count > 1) {
-                for (var ch : String.valueOf(count).toCharArray()) {
-                    chars[ptr++] = ch;
-                }
-            }
+           while(i < chars.length && chars[i] == current) {
+               count++;
+               i++;
+           }
 
-            count = 0;
-        }
-        return ptr;
+           if(count > 1) {
+               for(char ch: String.valueOf(count).toCharArray())
+                   chars[ptr++] = ch;
+           }
+
+           count = 0;
+       }
+
+       return ptr;
     }
 }

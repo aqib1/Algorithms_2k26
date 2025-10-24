19ca951e745f4fcc5fcb51e5b16b748a49561fd7
aqib javed <aqibbutt3078@gmail.com>
2025-10-24 15:32:43 +0100

    Leetcode 523 | Continous Sub Array sum

---
+++ src/main/java/com/algorithms/app/io/ripple/Leetcode523_ContinuousSubArraySum.java
@@ -0,0 +1,28 @@
+package com.algorithms.app.io.ripple;
+
+import java.util.HashMap;
+
+public class Leetcode523_ContinuousSubArraySum {
+    public static void main(String[] args) {
+        System.out.println(5 % 6);
+    }
+
+    // Time complexity O(n)
+    // Space complexity O(n)
+    public boolean checkSubarraySum(int[] nums, int k) {
+        var prefixModMap = new HashMap<Integer, Integer>();
+        var prefixMod = 0;
+        prefixModMap.put(0, -1);
+
+        for(int i = 0; i < nums.length; i++) {
+            prefixMod = (nums[i] + prefixMod) % k;
+            if(prefixModMap.containsKey(prefixMod)) {
+                if(i - prefixModMap.get(prefixMod) > 1)
+                    return true;
+            } else {
+                prefixModMap.put(prefixMod, i);
+            }
+        }
+        return false;
+    }
+}

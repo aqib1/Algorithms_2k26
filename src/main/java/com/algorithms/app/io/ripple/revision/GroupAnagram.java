aaab76f9ff9a9f26d7041ffc0fbee5488d057104
aqib javed <aqibbutt3078@gmail.com>
2025-10-27 19:05:21 +0000

    Group Anagrams

---
+++ src/main/java/com/algorithms/app/io/ripple/revision/GroupAnagram.java
@@ -0,0 +1,29 @@
+package com.algorithms.app.io.ripple.revision;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class GroupAnagram {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        if(strs.length == 0)
+            return List.of();
+        var anagramMap = new HashMap<String, List<String>>();
+        for(var str: strs) {
+            var key = getSortedKey(str);
+            if(anagramMap.containsKey(key)) {
+                anagramMap.get(key).add(str);
+            } else {
+                anagramMap.put(key, new ArrayList<>(List.of(str)));
+            }
+        }
+        return anagramMap.values().stream().toList();
+    }
+
+    private String getSortedKey(String str) {
+        var ch = str.toCharArray();
+        Arrays.sort(ch);
+        return String.valueOf(ch);
+    }
+}

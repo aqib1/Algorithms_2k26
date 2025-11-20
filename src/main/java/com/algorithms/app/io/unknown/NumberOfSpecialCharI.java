d8955e506c42e71e6f6bc469d8bebd8aef0211e1
Aqib <aqib@Aqibs-MacBook-Pro.local>
2025-11-20 00:27:53 +0000

    Algorithm

---
+++ src/main/java/com/algorithms/app/io/unknown/NumberOfSpecialCharI.java
@@ -0,0 +1,24 @@
+package com.algorithms.app.io.unknown;
+
+public class NumberOfSpecialCharI {
+    public int numberOfSpecialChars(String word) {
+        int count = 0;
+        boolean []upper = new boolean[26];
+        boolean []lower = new boolean[26];
+
+        for(char c : word.toCharArray()) {
+            if(Character.isUpperCase(c))
+                upper[c - 'A'] = true;
+            else
+                lower[c - 'a'] = true;
+        }
+
+        for(int i = 0; i < 26; i++) {
+            if(upper[i] && lower[i]) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}

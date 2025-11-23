caa10709a1f16627f1f251109912aed09a6de56c
Aqib <aqibbutt3078@gmail.com>
2025-11-23 00:46:33 +0000

    Floyd's Cycle detection

---
+++ src/main/java/com/algorithms/app/io/leetcode/FindDuplicatesUsingFloydCycle.java
@@ -0,0 +1,21 @@
+package com.algorithms.app.io.leetcode;
+
+public class FindDuplicatesUsingFloydCycle {
+    public int findDuplicate(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        var fast = nums[0]; var slow = nums[0];
+        do {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        } while(slow != fast);
+        slow = nums[0];
+
+        while(fast != slow) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+
+        return slow;
+    }
+}

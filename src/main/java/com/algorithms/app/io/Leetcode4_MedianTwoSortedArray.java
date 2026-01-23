8a0f46b3d6b514827bc6a538c3f51cf8601726e4
aqib javed <aqibbutt3078@gmail.com>
2026-01-23 02:33:38 +0000

    Leetcode4 | Median Two Sorted Array

--- src/main/java/com/algorithms/app/io/Leetcode4_MedianTwoSortedArray.java
+++ src/main/java/com/algorithms/app/io/Leetcode4_MedianTwoSortedArray.java
@@ -2,8 +2,60 @@ package com.algorithms.app.io;
 
 public class Leetcode4_MedianTwoSortedArray {
 
-    // Time complexity O(n)
-    // Space complexity O(n)
+    public static void main(String[] args) {
+        System.out.println(
+                findMedianFaster(new int[] {1, 2}, new int[] {3, 4})
+        );
+    }
+
+
+    // Time complexity O(log min(n,m))
+    // Space is O(1)
+    public static double findMedianFaster(int[] num1, int[] num2) {
+        if(num1.length > num2.length) {
+            var tmp = num1;
+            num1 = num2;
+            num2 = tmp;
+        }
+
+        int minIndex = 0, maxIndex = num1.length;
+        int x = 0, y = 0;
+        double median = 0.0;
+        boolean isOdd = (num1.length + num2.length) % 2 != 0;
+
+        while(minIndex <= maxIndex) {
+            x = (minIndex + maxIndex) / 2;
+            y = ((num1.length + num2.length + 1) / 2) - x;
+
+            if(x > 0 && y < num2.length && num1[x - 1] > num2[y]) {
+                maxIndex = x - 1;
+            } else if(y > 0 && x < num1.length && num2[y - 1] > num1[x]) {
+                minIndex = x + 1;
+            } else {
+                if(x == 0) {
+                    median = num2[y - 1];
+                } else if(y == 0) {
+                    median = num1[x - 1];
+                } else {
+                    median = Math.max(num1[x - 1], num2[y - 1]);
+                }
+                break;
+            }
+        }
+
+        if(isOdd)
+            return median;
+        if(x == num1.length)
+            return (median + num2[y]) / 2.0;
+        if(y == num2.length)
+            return (median + num1[x]) / 2.0;
+
+        return (median + Math.min(num1[x], num2[y])) / 2.0;
+    }
+
+    // Brute force
+    // Time complexity O(n + m)
+    // Space complexity O(n + m)
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         var merged = new int[nums1.length + nums2.length];
         int i = 0, j = 0, k = 0;

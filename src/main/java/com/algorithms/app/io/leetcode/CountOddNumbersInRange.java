570b6676cb7ad8ca2575c60c1ad9846421d7e75d
Aqib <aqibbutt3078@gmail.com>
2026-02-11 02:40:24 +0000

    Count odd numbers in range.

---
+++ src/main/java/com/algorithms/app/io/leetcode/CountOddNumbersInRange.java
@@ -0,0 +1,13 @@
+package com.algorithms.app.io.leetcode;
+
+public class CountOddNumbersInRange {
+
+    // Time complexity O(1) and space O(1)
+    public int countOdds(int low, int high) {
+        int totalNumbers = (high - low) + 1;
+        if(low % 2 != 0 && high % 2 != 0)
+            return (totalNumbers / 2) + 1;
+
+        return totalNumbers / 2;
+    }
+}
